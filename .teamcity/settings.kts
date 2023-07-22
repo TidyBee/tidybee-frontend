0c77f9bbe406a03dcd7f2a6b5aa77d90836e4aba
constantv <constantv@TeamCity>
2023-07-22 15:26:16 +0100

    TeamCity change in 'TidyUpSoftware Front' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,11 +1,29 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.nodeJS
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.pullRequests
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.PullRequests
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.perfmon
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2023.05"
 
@@ -20,11 +38,10 @@ object Build : BuildType({
     vcs {
         root(DslContext.settingsRoot)
     }
+
     steps {
         nodeJS {
-            shellScript = """
-                npm ci
-            """.trimIndent()
+            shellScript = "npm ci"
         }
     }
 
@@ -35,22 +52,6 @@ object Build : BuildType({
 
     features {
         perfmon {
-        }
-        pullRequests {
-            provider = github {
-                authType = vcsRoot()
-                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
-                ignoreDrafts = true
-            }
-        }
-        commitStatusPublisher {
-            publisher = github {
-                githubUrl = "https://api.github.com"
-                authType = personalToken {
-                    token = "******"
-                }
-            }
-            param("github_oauth_user", "Cavonstavant")
         }
     }
 })
