9d93a5fa40eaf2754de9f4372643c7be70d1aa3d
EmileEttel <emile.ettel@epitech.eu>
2023-09-10 20:04:28 +0200

    Hub Front communication: Added an modulable APIComponent that allows us to make calls to the API efficiently

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
     <MainPage />
+    <APIComponent :apiUrl="apiUrlTest">
+      <template v-slot="{ data }">
+        <div>
+          <p>Timezone: {{ data.timezone }}</p>
+          <p>Current Time: {{ data.datetime }}</p>
+          <p>Day of the Week: {{ data.day_of_week }}</p>
+        </div>
+      </template>
+    </APIComponent>
   </div>
 </template>
 
 <script>
 import MainPage from './MainPage.vue'
+import APIComponent from "./components/APIComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    MainPage
+    MainPage,
+    APIComponent
+  },
+  data() {
+    return {
+      apiUrlTest: 'http://worldtimeapi.org/api/timezone/Europe/Amsterdam',
+    };
   }
 }
 </script>
---
+++ src/components/APIComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="hasError">Error loading data</div>
+    <div v-else>
+      <slot :data="apiData"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      apiData: {},
+      isLoading: false,
+      hasError: false
+    }
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      this.isLoading = true;
+
+      fetch(this.apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          this.apiData = data;
+          console.log(this.apiData);
+          this.isLoading = false;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          this.hasError = true;
+          this.isLoading = false;
+        });
+    }
+  }
+}
+</script>
+  
