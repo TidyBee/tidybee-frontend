aac4b1b75224838c3b0f6e0fa1a7dbb3b0413bdd
EmileEttel <emile.ettel@epitech.eu>
2023-11-03 18:27:16 +0100

    help button : Created a Help Button component that opens a tutorial modal, still need to make sections in the modal for contacting and add proper tutorials to the modal

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -12,22 +12,42 @@
         </template>
       </APIComponent>
     </div>
+    <HelpButton @open="isOpen = !isOpen"/>
+    <BaseModal
+      :open="isOpen"
+      @close="isOpen = !isOpen"
+    >
+      <p>
+        This is where I would put the tutorial for tidyBee<br>
+        Once its written
+      </p>
+    </BaseModal>
   </div>
 </template>
 
 <script>
 import NavBar_Components from '@/components/NavBar.vue'
 import APIComponent from "./components/APIComponent.vue"
+import HelpButton from "@/components/HelpButton.vue"
+import BaseModal from '@/components/BaseModal.vue';
+import { ref } from 'vue';
 
 export default {
     name: 'MainPage',
     components: {
         NavBar_Components,
         APIComponent,
+        BaseModal,
+        HelpButton
+    },
+    setup() {
+      const isOpen = ref(false)
+
+      return { isOpen }
     },
     data() {
         return {
-            apiWorldTimeUrl: process.env.VUE_APP_API
+            apiWorldTimeUrl: process.env.VUE_APP_HUB
         };
     }
 };
---
+++ src/components/HelpButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="help-button"
+    @click="$emit('open')"
+  >
+    ?
+  </button>
+</template>
+
+<script>
+export default {
+    emits: ['open']
+}
+</script>
+
+<style scoped>
+.help-button {
+  position: fixed;
+  left: 20px;
+  bottom: 20px;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  font-size: larger;
+  cursor: pointer;
+  border: none;
+}
+</style>
