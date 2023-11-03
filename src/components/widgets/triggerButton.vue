273c2c8c18e28f404af330cb10b6ba397706df14
stom6 <tom.sarrazin@epitech.eu>
2023-11-03 11:19:57 -0700

    backend communication: rewrite sendData

--- src/components/widgets/triggerButton.vue
+++ src/components/widgets/triggerButton.vue
@@ -12,9 +12,7 @@ import { sendData } from '../../communication/communication.js';
 export default {
     methods: {
         triggerbtn() {
-          const url = "https://localhost:5000";
-          const data = { key1: "valeur1", key2: "valeur2" };
-          sendData(url, data);
+          sendData();
         },
     },
 }
