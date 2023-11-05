41e228a1184bf2507ff0dc2694834cef667216bf
stom6 <tom.sarrazin@epitech.eu>
2023-11-05 13:49:12 -0800

    rewriteCode: create css folders, add all styles to files defined in the css folders.

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -27,23 +27,4 @@ export default {
 }
 </script>
 
-<style scoped>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-
-.widget-container {
-    margin-left: 300px;
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.file-info {
-  margin-top: 20px;
-}
-</style>
+<style src="./css/MainPage.css" scoped></style>
