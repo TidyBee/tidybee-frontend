342b436bfddd25911a3691501a6a590a2040e26f
EmileEttel <emile.ettel@epitech.eu>
2023-11-20 00:15:56 +0100

    settingpagestructure: made sliding option into its own components, learned how to use emits effectively for clean code

---
+++ src/components/SliderOption.vue
@@ -0,0 +1,43 @@
+<template>
+  {{ option.name }} <br>
+  <input
+    v-model="value"
+    type="range"
+    min="0"
+    max="100"
+    class="slider"
+    @input="updateValue"
+  >
+  <br>
+  <input
+    v-model="value"
+    type="number"
+    @input="updateValue"
+  >
+</template>
+
+<script>
+export default {
+  name: "SliderOption",
+  props: {
+    option: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  emits: ['configInput'],
+  data() {
+    return {
+        value: 50,
+    }
+  },
+  async mounted() {
+    this.value = this.option.value;
+  },
+  methods: {
+    updateValue() {
+      this.$emit("configInput", this.value);
+    },
+  }
+}
+</script>
