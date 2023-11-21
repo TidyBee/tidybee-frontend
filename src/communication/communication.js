9f446a90704a68c17df7dcb74f00fda5c5e3d68f
stom6 <tom.sarrazin@epitech.eu>
2023-11-21 11:28:02 -0800

    codeMoreGenereic: replace old error handling and replace by an more generic

--- src/communication/communication.js
+++ src/communication/communication.js
@@ -2,18 +2,16 @@ import axios from 'axios'
 
 export async function fetchData(url) {
   try {
+    console.log('url:', url);
     const response = await axios.get(url);
-    if (response.status === 200) {
-      if (response.data && typeof response.data === 'object') {
-        return response.data;
-      } else {
-        console.error('Invalid data received:', response.data);
-        return null;
-      }
-    } else {
-      console.error('Error HTTP :', response.status, response.statusText);
-      return null;
-    }
+    const isDataValid = response?.status === 200 && response?.data && typeof response.data === 'object';
+    const errorMessage = response?.status !== 200 ? `Error HTTP: ${response?.status} ${response?.statusText || ''}` : 'Invalid data received';
+
+    console[isDataValid ? 'log' : 'error'](
+      isDataValid ? 'Data fetched successfully' : errorMessage
+    );
+
+    return isDataValid ? response.data : null;
   } catch (error) {
     console.error('Data recovery error:', error.message);
     return null;
@@ -22,20 +20,16 @@ export async function fetchData(url) {
 
 export async function postData(url, data) {
   try {
-    if (url && data && typeof data === 'object') {
-      const response = await axios.post(url, data);
+    const isValidInput = url && data && typeof data === 'object';
+    const response = isValidInput ? await axios.post(url, data) : null;
 
-      if (response.status === 200) {
-        console.log('Data sent successfully:', response.data);
-        return 'success';
-      } else {
-        console.error('Error HTTP :', response.status, response.statusText);
-        return 'error';
-      }
-    } else {
-      console.error('Invalid URL or data.');
-      return 'error';
-    }
+    const isSuccess = response && response.status === 200;
+
+    console[isSuccess ? 'log' : 'error'](
+      isSuccess ? `Data sent successfully: ${response.data}` : `Error HTTP: ${response?.status} ${response?.statusText || ''}` || 'Invalid URL or data.'
+    );
+
+    return isSuccess ? 'success' : 'error';
   } catch (error) {
     console.error('Error sending data:', error.message);
     return 'error';
