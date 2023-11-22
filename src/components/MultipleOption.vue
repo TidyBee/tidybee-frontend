29182d48518c8bb646b35062b828b7b3163e4805
EmileEttel <emile.ettel@epitech.eu>
2023-11-22 11:31:27 +0100

    settingpagestructure: Made the rest of the options into their own components and made sure that the configuration page receives their updates

---
+++ src/components/MultipleOption.vue
@@ -0,0 +1,43 @@
+<template>
+  {{ option.name }} <br>
+  <ul>
+    <li
+      v-for="choices in value"
+      :key="choices.name"
+    >
+      {{ choices.name }}
+      <input
+        v-model="choices.checked"
+        type="checkbox"
+        @input="updateValue"
+      >
+    </li>
+  </ul>
+</template>
+  
+  <script>
+  export default {
+    name: "MultipleOption",
+    props: {
+      option: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+    emits: ['configInput'],
+    data() {
+      return {
+          value: [''],
+      }
+    },
+    async mounted() {
+      this.value = this.option.value;
+    },
+    methods: {
+      updateValue() {
+        this.$emit("configInput", this.value);
+      },
+    }
+  }
+  </script>
+  
