c6cf654ab9f86b2d51a3356ad90568d720f4ce73
stom6 <tom.sarrazin@epitech.eu>
2023-11-22 09:11:56 -0800

    triggerbutton: replace fix url by .env url and import triggerButton

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -2,7 +2,7 @@
   <div>
     <NavBar />
     <FileList
-      :tidy-hub-api="'https://localhost:7114/api/Dashboard/top-heaviest-files'"
+      :tidy-hub-api="tidyHubApi + 'api/Dashboard/top-heaviest-files'"
       :widget-name="'Top Heaviest Files'"
     />
     <triggerbtn />
@@ -12,16 +12,19 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 import FileList from "@/components/widgets/fileList.vue"
+import triggerbtn from '@/components/widgets/triggerButton.vue'
 
 export default {
   name: 'MainPage',
   components: {
       NavBar,
       FileList,
+      triggerbtn,
   },  
   data() {
     return {
       filesInfos: [],
+      tidyHubApi: process.env.VUE_APP_HUB,
     };
   },
 }
