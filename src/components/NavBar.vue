1fe0879452bca03b3f8f7ed26280b74f5d65ce24
EmileEttel <emile.ettel@epitech.eu>
2023-11-25 12:06:42 +0100

    vuetify: Made navbar into a vuetify component and moved it to app.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="NavBar">
+  <v-app-bar class="px-3" flat density="compact">
+    <v-app-bar-icon>
+      <v-icon> </v-icon>
+    </v-app-bar-icon>
+    <v-spacer></v-spacer>
+    <v-tabs centered color="grey-darken-2">
+      <v-tab
+        v-for="link in links"
+        :key="link"
+        :text="link"
+        @click="redirectToRoute(link)"
+      ></v-tab>
+    </v-tabs>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+  <!-- <div class="NavBar">
     <div class="logo" @click="redirectToRoute('Home')">
       <img src="@/assets/tidybeelogo.png" alt="TidyBeeLogo" />
       <h2 class="nav-item">TidyBee</h2>
@@ -10,12 +25,14 @@
       </h3>
       <h3 class="nav-item">À Propos</h3>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
 export default {
-  name: "NavBar",
+  data: () => ({
+    links: ["Home", "Configuration", "About"],
+  }),
   methods: {
     redirectToRoute(routeName) {
       this.$router.push({ name: routeName });
@@ -23,5 +40,3 @@ export default {
   },
 };
 </script>
-
-<style src="./css/NavBar.css" scoped></style>
