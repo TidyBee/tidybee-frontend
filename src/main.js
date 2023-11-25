6a881c0e0e0841e2b32d8d6c7c2ad4af875c4469
majent4 <99695392+majent4@users.noreply.github.com>
2023-11-25 11:41:54 +0100

    vuetify: add package and mainpage wireframe

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/routers.js";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).use(router).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).use(router).mount("#app");
