7649ffa9712ff3d8989d69e4de39807d12f5e9c9
EmileEttel <emile.ettel@epitech.eu>
2023-11-29 15:05:44 +0100

    vuetify: Made configuration page into vuetify components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,18 @@
 <template>
   <v-app-bar class="px-3" flat density="compact">
     <v-app-bar-icon>
-      <v-icon> </v-icon>
+      <v-img src="tidybeelogo.png" />
     </v-app-bar-icon>
-    <v-spacer></v-spacer>
+    <v-spacer />
     <v-tabs centered color="grey-darken-2">
       <v-tab
         v-for="link in links"
         :key="link"
-        :text="link"
+        :text="$t('navbar.' + link)"
         @click="redirectToRoute(link)"
       ></v-tab>
     </v-tabs>
-    <v-spacer></v-spacer>
-  </v-app-bar>
-  <!-- <div class="NavBar">
-    <div class="logo" @click="redirectToRoute('Home')">
-      <img src="@/assets/tidybeelogo.png" alt="TidyBeeLogo" />
-      <h2 class="nav-item">TidyBee</h2>
-    </div>
+    <v-spacer />
     <select v-model="locale" @change="switchLang">
       <option
         v-for="availableLocale in availableLocales"
@@ -27,6 +21,12 @@
         {{ availableLocale }}
       </option>
     </select>
+  </v-app-bar>
+  <!-- <div class="NavBar">
+    <div class="logo" @click="redirectToRoute('Home')">
+      <img src="@/assets/tidybeelogo.png" alt="TidyBeeLogo" />
+      <h2 class="nav-item">TidyBee</h2>
+    </div>
     <div class="menu-items">
       <h3 class="nav-item" @click="redirectToRoute('Configuration')">
         {{ $t("navbar.configuration") }}
