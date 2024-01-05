767ac1c3554849a52e24c9fb16e24ebce4f7bb71
stom6 <tom.sarrazin@epitech.eu>
2024-01-05 10:05:10 +0100

    widgetPanel: implemetation of adding and removing widgets

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -3,37 +3,66 @@
     <v-container>
       <v-row>
         <v-col cols="12" md="2">
-          <v-sheet rounded="lg">
+          <v-row>
             <FileList
+              v-if="Heaviest"
               :tidy-hub-api="tidyHubApi + 'api/Dashboard/top-heaviest-files'"
               :widget-name="'Top Heaviest Files'"
+              @click="handleFileListClick('Heaviest')"
             />
-          </v-sheet>
+          </v-row>
+          <v-row>
+            <FileList
+              v-if="Unused"
+              :tidy-hub-api="tidyHubApi + 'api/Dashboard/top-heaviest-files'"
+              :widget-name="'Top Unused Files'"
+              @click="handleFileListClick('Unused')"
+          />
+        </v-row>
         </v-col>
       </v-row>
+      <panel-widget @toggle-widget="handleToggleWidget" />
     </v-container>
   </v-main>
 </template>
 
-
 <script>
+import PanelWidget from "@/components/widgets/Panel.vue";
 import FileList from "@/components/widgets/FileList.vue";
-// import PostButton from "@/components/widgets/PostButton.vue";
 
 export default {
   name: "MainPage",
   components: {
+    PanelWidget,
     FileList,
-    // PostButton,
   },
   data() {
     return {
       filesInfos: [],
       tidyHubApi: process.env.VUE_APP_HUB,
+      Heaviest: false,
+      Unused: false,
     };
+  },
+  methods: {
+    handleFileListClick(widgetName) {
+      if (widgetName === 'Heaviest') {
+        this.Heaviest = false;
+      } else if (widgetName === 'Unused') {
+        this.Unused = false;
+      }
+    },
+    handleToggleWidget(status, widgetName) {
+      console.log("HERE");
+      if (widgetName === 'Heaviest') {
+        this.Heaviest = status;
+      } else if (widgetName === 'Unused') {
+        this.Unused = status;
+      }
+    }
   },
 };
 </script>
 
-
-<style src="@/css/MainPage.css" scoped></style>
+<style scoped>
+</style>
---
+++ src/components/widgets/Panel.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <v-btn color="grey" @click="openDialog(1)">+</v-btn>
+
+    <v-dialog v-model="dialog1" max-width="400">
+      <v-card>
+        <v-card-title>
+          Widget Panel
+        </v-card-title>
+        <v-card-text>
+          <div v-for="(button, index) in buttonTexts" :key="index">
+            <v-btn class="mb-3 elevate" @click="openDialog(2, index + 1), closeDialog(1)">
+              {{ button }}
+            </v-btn>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="closeDialog(1)">Fermer</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialog2" max-width="400">
+      <v-card>
+        <v-card-title>
+          {{ buttonTexts[selectedButton - 1] }}
+        </v-card-title>
+        <v-btn class="mb-3 elevate" @click="handleToggleWidget(widgetName[selectedButton - 1])">
+          3x4
+        </v-btn>
+        <v-card-actions>
+          <v-btn color="primary" @click="closeDialog(2), openDialog(1)">Retour</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PanelWidget",
+  data() {
+    return {
+      dialog1: false,
+      dialog2: false,
+      selectedButton: null,
+      buttonTexts: [
+        "Top Heaviest File",
+        "Top Unused",
+        "Dashboard Dossier",
+        "Top Bad Named",
+        "Widget de Tri",
+        "Consomation Co2",
+      ],
+      widgetName: [
+        "Heaviest",
+        "Unused",
+        "Dashboard",
+        "Badnamed",
+        "Tri",
+        "Co2",
+      ],
+    };
+  },
+  methods: {
+    openDialog(dialogNumber, buttonIndex) {
+      if (dialogNumber === 1) {
+        this.selectedButton = null;
+        this.dialog1 = true;
+      } else if (dialogNumber === 2) {
+        this.selectedButton = buttonIndex;
+        this.dialog2 = true;
+      }
+    },
+    closeDialog(dialogNumber) {
+      if (dialogNumber === 1) {
+        this.dialog1 = false;
+      } else if (dialogNumber === 2) {
+        this.dialog2 = false;
+      }
+    },
+    handleToggleWidget(widgetName) {
+      this.$emit("toggle-widget", true, widgetName);
+      this.dialog1 = false;
+      this.dialog2 = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mb-3 {
+  margin-bottom: 15px;
+}
+</style>
