7c669281bd8ea871c1a3f37084828bbea8b19305
EmileEttel <emile.ettel@epitech.eu>
2024-02-08 00:17:10 +0100

    add unittests: placed testable function inside utils and made tests for it.

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,29 @@ export const getGrade = (tidyScore) => {
   score += tidyScore?.duplicated == true;
   return scores[score];
 };
+
+export const formatFileSize = (fileSize) => {
+  const sizeThresholds = [
+    [1024, "B"],
+    [Math.pow(1024, 2), "KB"],
+    [Math.pow(1024, 3), "MB"],
+    [Math.pow(1024, 4), "GB"],
+  ];
+  if (typeof(fileSize) != "number")
+    return "NaN";
+  let fixed = 0;
+  for (const [threshold, unit] of sizeThresholds) {
+    if (fileSize < threshold) {
+      return (fileSize / (threshold / 1024)).toFixed(fixed) + " " + unit;
+    }
+    fixed = 2;
+  }
+  return (
+    (
+      fileSize /
+      (sizeThresholds[sizeThresholds.length - 1][0] / 1024)
+    ).toFixed(2) +
+    " " +
+    sizeThresholds[sizeThresholds.length - 1][1]
+  );
+};
