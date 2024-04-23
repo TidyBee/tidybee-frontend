9f990d22972512e3062f5ddc9b5c839b6566f5bd
EmileEttel <emile.ettel@epitech.eu>
2024-04-23 17:32:46 +0200

    refactoHeader: Switched logo, made it clickable, added shadow to navbar, made navbar thicker to fit logo

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -1,15 +1,16 @@
 <template>
   <v-app-bar-icon class="logo">
-    <img src="@/assets/tidybee-logo.svg" alt="TidyBee Logo" height="150" />
+    <img src="@/assets/tidybee-logo.svg" alt="TidyBee Logo" height="150" @click="redirectToRoute('Home')" />
   </v-app-bar-icon>
-  <v-app-bar class="px-3" flat density="compact">
+  <v-app-bar class="px-3" density="compact" :elevation="5" :height="80">
     <v-spacer />
-    <v-tabs centered color="grey-darken-2">
+    <v-tabs centered color="grey-darken-2" :grow="true">
       <v-tab
         v-for="link in links"
         :key="link"
         :text="$t('navbar.' + link)"
         :data-cy="link"
+        class="tab"
         @click="redirectToRoute(link)"
       ></v-tab>
     </v-tabs>
