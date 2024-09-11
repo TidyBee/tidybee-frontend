b423582800bb8524f9a95c8aa44be24446f2197a
stom6 <tom.sarrazin@epitech.eu>
2024-09-11 22:38:21 +0200

    fix configuration lentgh error and fix change tab isOpen still open

--- src/components/communication/ApiLoader.vue
+++ src/components/communication/ApiLoader.vue
@@ -71,7 +71,6 @@ export default {
         try {
           const parsedData = JSON.parse(data);
           if (parsedData) {
-            console.log("parsedata :", parsedData);
             this.apiData = parsedData;
             this.isLoading = false;
             this.hasError = false;
