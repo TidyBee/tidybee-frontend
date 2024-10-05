dd238beee954d8909e59b5cbbdc97cd555032c84
Tesnier Lucas <72015360+LucasTesnier@users.noreply.github.com>
2024-10-05 15:15:26 +0200

    Update IntegrationWidget.vue

--- src/components/Settings/IntegrationWidget.vue
+++ src/components/Settings/IntegrationWidget.vue
@@ -48,7 +48,7 @@ export default {
   },
   created() {
     // Initialize the gRPC client
-    this.grpcClient = new NotionSyncClient("http://localhost:5057", null, null); /// DID NOT SUCESS TO USE THE .ENV WEIRD
+    this.grpcClient = new NotionSyncClient("http://localhost:8081", null, null); /// DID NOT SUCESS TO USE THE .ENV WEIRD
   },
   methods: {
     linkDatabase() {
