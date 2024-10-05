f11e3ba859204c87e59d04078329f28407402e6c
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-10-05 18:42:45 +0200

    revert back hub url to localhost for testing

--- src/components/communication/ApiLoader.vue
+++ src/components/communication/ApiLoader.vue
@@ -63,7 +63,7 @@ export default {
   methods: {
     setupWebSocket() {
       this.connection = new signalR.HubConnectionBuilder()
-        .withUrl("http://prod.tidybee.fr:7003/widgetHub")
+        .withUrl("http://localhost:7003/widgetHub")
         .configureLogging(signalR.LogLevel.Information)
         .build();
 
