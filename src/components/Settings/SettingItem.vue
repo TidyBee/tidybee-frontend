06669e1b45707b6119746d7c8bb6aba7e5df9955
EmileEttel <emile.ettel@epitech.eu>
2024-10-05 18:33:39 +0200

    new-config: Changed up settings page to match with new figma wireframe, made weight into progress bar, added search bar.

--- src/components/Settings/SettingItem.vue
+++ src/components/Settings/SettingItem.vue
@@ -1,26 +1,23 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <div
-        class="text-left ml-2"
-        :data-cy="(`config.${config.name}`)"
-      >
-        {{ config.name }}
-      </div>
-      <div class="text-left pt-2 pb-4 ml-2 text-grey-darken-1 text-no-wrap text-caption ml-2" data-cy="tidyscore-description">
-        {{ $t(`settings.description`) + config.description }}
-        <!-- <br v-if="config.weight"> &nbsp;&nbsp;
-        <a v-if="config.weight"> {{ $t(`settings.weight`) + config.weight }} </a>
-        <br v-if="config.regex"> &nbsp;&nbsp;
-        <a v-if="config.regex"> {{ $t(`settings.regex`) + config.regex }} </a>
-        <br v-if="config.limitISO"> &nbsp;&nbsp;
-        <a v-if="config.limitISO"> {{ $t(`settings.limitISO`) + config.limitISO }} </a>
-        <br v-if="config.limitInt"> &nbsp;&nbsp;
-        <a v-if="config.limitInt"> {{ $t(`settings.limitInt`) + config.limitInt }} </a> -->
-      </div>
-    </v-col>
-  </v-row>
-  <v-divider></v-divider>
+  <v-card class="setting-item">
+    <div class="text-left ml-2" :data-cy="(`config.${config.name}`)" @click="isOpen = !isOpen">
+      {{ config.name }}
+    </div>
+    <div
+      v-if="isOpen" class="text-left pt-2 pb-2 ml-2 text-grey-darken-1 text-no-wrap text-caption ml-2"
+      data-cy="tidyscore-description"
+    >
+      {{ $t(`settings.description`) + config.description }}
+      <br v-if="config.weight"> &nbsp;&nbsp;
+      <a v-if="config.weight" class="d-flex align-center"> {{ $t(`settings.weight`) }} &nbsp;&nbsp;
+        <v-progress-linear
+          bg-color="#000" color="#12512a" height="12" max="3" min="0" :model-value="config.weight"
+          rounded
+        />
+      </a>
+    </div>
+    <v-icon class="item-chevron" :icon="isOpen ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="isOpen = !isOpen" />
+  </v-card>
 </template>
 
 <script>
@@ -40,6 +37,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped src="@/../css/components/Settings/SettingItem.css"></style>
