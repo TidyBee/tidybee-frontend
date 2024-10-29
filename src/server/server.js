ff52d02fc16427ed0855b643511366db3f935741
stom6 <tom.sarrazin@epitech.eu>
2024-10-29 08:16:39 +0100

    step 2

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 app.listen(port, () => {
