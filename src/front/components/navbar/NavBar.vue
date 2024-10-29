63ce429a75f9247d16e243317c79e0e156dc648d
stom6 <tom.sarrazin@epitech.eu>
2024-10-29 22:28:15 +0100

    feat: Navbar in right place & handle application theme

---
+++ src/front/components/navbar/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app-bar class="navbar" :elevation="3" app>
+    <v-spacer />
+      <v-tab 
+        height="80"
+        :text="'TidyList'" 
+        @click="redirectToRoute('Overview')"
+      ></v-tab>
+      <v-tab 
+        height="80"
+        :text="'Statistique'" 
+        @click="redirectToRoute('Overview')"
+      ></v-tab>
+      <v-tab
+        :text="'Home'"
+        @click="redirectToRoute('Home')"
+        class="d-flex align-center justify-center"
+      >
+        <v-img
+          src="https://tidybee.fr/images/logo-bookmark.svg"
+          height="80"
+          width="120"
+          contain
+        ></v-img>
+      </v-tab>
+      <v-tab 
+        height="80"
+        :text="'TITLE'" 
+        @click="redirectToRoute('Overview')"
+        ></v-tab>
+      <v-tab 
+        height="80"
+        :text="'Configuration'" 
+        @click="redirectToRoute('Configuration')"
+        ></v-tab>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+import VueCookies from "vue-cookies";
+import { ref } from 'vue';
+
+export default {
+  name: "NavBar",
+  setup() {
+    if (!VueCookies.get("locale")) {
+      VueCookies.set("locale", "fr");
+    }
+    const locale = VueCookies.get("locale");
+    const activeTab = ref(2);
+
+    return {
+      locale,
+      activeTab,
+    };
+  },
+  data() {
+    return {
+      availableLocales: [],
+      links: ["Home", "Configuration", "Overview"],
+    };
+  },
+  mounted() {
+    this.availableLocales = this.$i18n.availableLocales;
+  },
+  methods: {
+    redirectToRoute(routeName) {
+      this.$router.push({ name: routeName });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/front/components/themeButton/ThemeButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="button-container">
+    <v-switch
+      v-model="isDarkTheme"
+      @change="toggleTheme"
+      inset
+      hide-details
+    />
+  </div>
+</template>
+
+<script>
+import { useTheme } from 'vuetify';
+import { computed } from 'vue';
+
+export default {
+  name: "ThemeButton",
+  setup() {
+    const theme = useTheme();
+    const isDarkTheme = computed(() => theme.global.name.value === 'dark');
+
+    const toggleTheme = () => {
+      theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light';
+    };
+
+    return {
+      toggleTheme,
+      isDarkTheme,
+    };
+  },
+}
+</script>
+
+<style scoped>
+.button-container {
+  position: fixed;
+  bottom: 20px !important;
+  right: 20px !important;
+  z-index: 1000;
+}
+</style>
