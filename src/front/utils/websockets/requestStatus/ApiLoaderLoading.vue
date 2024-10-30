59d6b87303a50933b1545b5af0b0ca6ce324b72b
stom6 <tom.sarrazin@epitech.eu>
2024-10-30 08:16:29 +0100

    refacto: add websocket

---
+++ src/front/utils/websockets/requestStatus/ApiLoaderLoading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="full-height d-flex justify-center align-center">
+    <div class="widget-title">{{ $t('dashboard.widgets.' + widgetName) }}</div>
+    <v-progress-circular indeterminate :size="80" :width="12"></v-progress-circular>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: ApiLoaderLoading,
+  props: {
+    widgetName: {
+      type: String,
+      required: true,
+    },
+  };
+};
+</script>
+
+<style src=""/>
