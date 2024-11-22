3bef7086ddd323c9d3f5143aa1a0efc4dc1de00e
Emile Ettel <emile.ettel@epitech.eu>
2024-11-22 16:26:03 +0100

    Readded old config page with new features, cleaned up integration buttons and settable severity settings.

--- src/front/pages/configuration/ConfigurationPage.vue
+++ src/front/pages/configuration/ConfigurationPage.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
-    ConfigurationPage
-  </div>
+  <v-container fluid class="pt-16">
+    <v-row justify="center">
+      <ConfigurationWidget class="widget-top-container" :tidy-hub-api="'SendTidyRules'" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import ConfigurationWidget from "./components/ConfigurationWidget.vue";
+
 export default {
   name: "ConfigurationPage",
+  components: {
+    ConfigurationWidget,
+  },
+  data() {
+    return {
+      tidyHubApi: process.env.VUE_APP_HUB,
+    };
+  },
 };
 </script>
 
-<style scoped>
-</style>
+<style src="./ConfigurationPage.css" scoped></style>
---
+++ src/front/pages/configuration/components/ConfigurationWidget.vue
@@ -0,0 +1,145 @@
+<template>
+    <v-card class="rounded-rectangle" elevation="10" :data-cy="`settings-container`">
+        <ApiLoader :api-url="tidyHubApi" class="full-height">
+            <template #default="{ data }">
+                <div v-if="data" class="full-height">
+                    <v-card class="rounded-rectangle-settings-title full-height" :data-cy="`settings-title-container`">
+                        <v-tabs direction="vertical" :hide-slider="true" class="full-height">
+                            <div class="upper-tab-border">
+                                <v-tab v-for="(rule, index) in data.rules" :key="index" @click="tab = rule.name"
+                                    class="rule-tab">
+                                    {{ $t('settings.rule.' + rule.name) }}
+                                </v-tab>
+                            </div>
+                            <div class="upper-tab-border">
+                                <v-tab class="rule-tab" @click="severityDialog = true">
+                                    Sévérité des règles
+                                </v-tab>
+                            </div>
+                            <div class="bottom-tab-border">
+                                <IntegrationWidget />
+                            </div>
+                        </v-tabs>
+                    </v-card>
+                    <v-card class="rounded-rectangle-settings-content" :data-cy="`settings-content-container`">
+                        <div v-if="selectedRule(data)">
+                            <v-row>
+                                <v-virtual-scroll :height="650" style="margin-top: 15px"
+                                    :items="selectedRule(data).configurations">
+                                    <template #default="{ item: config }">
+                                        <v-list-item cols="12">
+                                            <SettingItem :config="config" />
+                                        </v-list-item>
+                                    </template>
+                                </v-virtual-scroll>
+                            </v-row>
+                        </div>
+                    </v-card>
+                </div>
+                <v-dialog v-model="severityDialog" max-width="600px">
+                    <SeverityWidget :rules="data.rules" @close="severityDialog = false" />
+                </v-dialog>
+                <!-- <v-dialog >
+                    <v-card class="rounded-dialog">
+                        <v-card-title>
+                            Sévérité des règles
+                        </v-card-title>
+                        <v-card-text>
+                            <p>
+                                Veuillez choisir le degré de sévérité des règles.
+                            </p>
+                            <p>
+                                La sévérité a impact direct sur le score de vos fichiers.
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <v-container>
+                                <v-row>
+                                    <v-col />
+                                    <v-col class="text-center">
+                                        Faible
+                                    </v-col>
+                                    <v-col class="text-center">
+                                        Moyen
+                                    </v-col>
+                                    <v-col class="text-center">
+                                        Elevé
+                                    </v-col>
+                                </v-row>
+                                <br />
+                                <v-radio-group v-for="(rule, index) in data.rules" :key="index" v-model="rule.weight">
+                                    <v-row>
+                                        <v-col class="text-center grid-items">
+                                            {{ $t('settings.rule.' + rule.name) }}
+                                        </v-col>
+                                        <v-col class="text-center grid-items">
+                                            <v-radio :value='1' />
+                                        </v-col>
+                                        <v-col class="text-center grid-items">
+                                            <v-radio :value='2' />
+                                        </v-col>
+                                        <v-col class="text-center grid-items">
+                                            <v-radio :value='3' />
+                                        </v-col>
+                                    </v-row>
+                                </v-radio-group>
+                            </v-container>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn text @click="severityDialog = false">Annuler</v-btn>
+                            <v-btn text @click="severityDialog = false">Soumettre</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog> -->
+            </template>
+        </ApiLoader>
+    </v-card>
+</template>
+
+<script>
+import ApiLoader from "@/front/utils/websockets/ApiLoader.vue";
+import SettingItem from "./SettingItem.vue";
+import IntegrationWidget from "./IntegrationWidget.vue";
+import SeverityWidget from "./SeverityWidget.vue";
+
+export default {
+    name: "ConfigurationWidget",
+    components: {
+        ApiLoader,
+        SettingItem,
+        IntegrationWidget,
+        SeverityWidget
+    },
+    props: {
+        tidyHubApi: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            tab: "misnamed",
+            selectedTabLabel: "MISNAMED",
+            severityDialog: false,
+        };
+    },
+    computed: {
+        selectedRule() {
+            return (data) => {
+                console.log(data);
+                if (data && data.rules) {
+                    if (this.tab)
+                        return data.rules.find(rule => rule.name === this.tab);
+                    else
+                        return data.rules.find(rule => rule.name === 'misnamed');
+                }
+                return null;
+            };
+        }
+    },
+};
+</script>
+
+<style scoped src="./ConfigurationWidget.css"></style>
