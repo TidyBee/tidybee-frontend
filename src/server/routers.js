26eb95992e82be3ad6c121730be8185e93f6ec81
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2025-01-22 00:15:12 +0100

    Update routers.js

--- src/server/routers.js
+++ src/server/routers.js
@@ -34,7 +34,7 @@ const routes = [
     path: "/callback/:provider",
     redirect: async to => {
       if (to.params.provider == "google") {
-        const Oauth2Token = to.hash.split('=')[1];
+        const Oauth2Token = to.hash.split('=')[1].split("&token_type")[0];
         const req = await fetch(`${process.env.VUE_APP_HUB_AUTH}/Cloud/SyncGoogle`, {
           method: 'POST',
           headers: {
